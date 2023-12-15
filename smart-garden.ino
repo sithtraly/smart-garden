bfde9635452d7428ed6ffb28fc15bdff8f8c4b3c
Sith Traly <sithtraly147@gmail.com>
2023-12-15 08:52:21 +0700

    Connect Water Pump Mini 3-6V

--- smart-garden.ino
+++ smart-garden.ino
@@ -1,12 +1,11 @@
 #include <SoftwareSerial.h>
 
-int state = 0;
 int rx = 0;
 int tx = 1;
 SoftwareSerial mySerial(rx, tx); // RX, TX
 char command;
 int ledPin = 10;
-
+int relayPin = 2;
 
 void setup() {
   // put your setup code here, to run once:
@@ -17,12 +16,14 @@ void setup() {
   mySerial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(ledPin, OUTPUT);
+  pinMode(relayPin, OUTPUT);
 }
 
 void loop() {
   if (mySerial.available() > 0) {
     command = mySerial.read();
     command == '1' ? digitalWrite(ledPin, HIGH) : digitalWrite(ledPin, LOW);
+    command == '0' ? digitalWrite(relayPin, HIGH) : digitalWrite(relayPin, LOW);
     Serial.println(command);
   }
   delay(100);
